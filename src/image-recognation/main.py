927ac4082b888c0d7ef2aee303e57e0954329d26
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-09-24 00:54:46 +0300

    Finish image recognation

--- src/image-recognation/main.py
+++ src/image-recognation/main.py
@@ -9,10 +9,20 @@ RESOURCES_DIR = os.path.normpath(
 
 def main():
     kpi_image_path = os.path.join(RESOURCES_DIR, "kpi.jpg")
-    original_image = cv2.imread(kpi_image_path, cv2.IMREAD_GRAYSCALE)
+    original_image = cv2.imread(kpi_image_path, cv2.IMREAD_COLOR)
 
-    blured = cv2.GaussianBlur(original_image, (3, 3), 0)
-    edged = cv2.Canny(blured, 100, 200)
+    gray = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    blured = cv2.GaussianBlur(gray, (3, 3), 0)
+    edged = cv2.Canny(blured, 10, 500)
+    contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    rectangles = []
+    for contour in contours:
+        perimiter = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.02 * perimiter, True)
+        if len(approx) == 4:
+            rectangles.append(approx)
+    cv2.drawContours(original_image, rectangles, -1, (0, 255, 0), 1)
 
     cv2.imshow("KPI", original_image)
     cv2.imshow("KPI Processed", edged)
